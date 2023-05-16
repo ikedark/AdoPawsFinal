e6efc6075ce8d278b639370fee3d034b43604e9d
Elmamis69 <60937298+Elmamis69@users.noreply.github.com>
2023-05-16 03:05:25 -0700

    Mapa y ubicaciones

--- app/src/main/java/mx/edu/potros/adopaws/MapaPrueba.kt
+++ app/src/main/java/mx/edu/potros/adopaws/MapaPrueba.kt
@@ -1,11 +1,237 @@
 package mx.edu.potros.adopaws
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.GoogleMap.OnMyLocationButtonClickListener
+import com.google.android.gms.maps.GoogleMap.OnMyLocationClickListener
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.PolylineOptions
+import kotlin.math.E
 
-class MapaPrueba : AppCompatActivity() {
+class MapaPrueba : AppCompatActivity(), OnMapReadyCallback, OnMyLocationButtonClickListener, OnMyLocationClickListener {
+
+    private lateinit var map: GoogleMap
+
+    companion object {
+        const val REQUEST_CODE_LOCATION = 0
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+        createMarker()
+        map.setOnMyLocationButtonClickListener(this)
+        map.setOnMyLocationClickListener(this)
+        enableLocation()
+    }
+
+    private fun createMarker() {
+        val coordinates = LatLng(27.496218, -109.971497)
+        val coordinates2 = LatLng(27.482605, -109.957548)
+        val coordinates3 = LatLng(27.491404, -109.977105)
+        val coordinates4 = LatLng(27.514345, -109.942738)
+        val coordinates5 = LatLng(27.491888, -109.934804)
+        val coordinates6 = LatLng(27.474226, -109.942507)
+        val coordinates7 = LatLng(27.449442, -109.922981)
+        val coordinates8 = LatLng(27.497741, -109.939852)
+        val coordinates9 = LatLng(27.494692, -109.945279)
+        val coordinates10 = LatLng(27.470737, -109.963245)
+        val marker = MarkerOptions().position(coordinates).title("Veterinaria Nainari")
+        val marker2 = MarkerOptions().position(coordinates2).title("Veterinaria 200")
+        val marker3= MarkerOptions().position(coordinates3).title("Veterinaria Casa blanca")
+        val marker4= MarkerOptions().position(coordinates4).title("Veterinaria Norte")
+        val marker5= MarkerOptions().position(coordinates5).title("Veterinaria Centro")
+        val marker6= MarkerOptions().position(coordinates6).title("Veterinaria MEDICANS")
+        val marker7= MarkerOptions().position(coordinates7).title("Veterinaria MAF")
+        val marker8= MarkerOptions().position(coordinates8).title("Veterinaria Dr. Hiram")
+        val marker9= MarkerOptions().position(coordinates9).title("Veterinaria Dr. Zavala")
+        val marker10= MarkerOptions().position(coordinates10).title("Veterinaria La chiquita")
+        map.addMarker(marker)
+        map.addMarker(marker2)
+        map.addMarker(marker3)
+        map.addMarker(marker4)
+        map.addMarker(marker5)
+        map.addMarker(marker6)
+        map.addMarker(marker7)
+        map.addMarker(marker8)
+        map.addMarker(marker9)
+        map.addMarker(marker10)
+        map.animateCamera(
+            CameraUpdateFactory.newLatLngZoom(coordinates, 15f),
+            1000,
+            null
+        )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mapa_prueba)
+        createMapFragment()
+
+        val btnHome: ImageButton = findViewById(R.id.btn_home)
+        val btnMapa: ImageButton = findViewById(R.id.btn_map)
+        val btnAvisos: ImageButton = findViewById(R.id.btn_warnings)
+        val btnMensajes: ImageButton = findViewById(R.id.btn_messages)
+        val btnProfile: ImageButton = findViewById(R.id.btn_profile)
+
+        btnHome.setOnClickListener {
+            val intent: Intent = Intent(this, Home::class.java)
+            startActivity(intent)
+        }
+
+        btnAvisos.setOnClickListener {
+            val intent: Intent = Intent(this, Encontrar_Mascota::class.java)
+            startActivity(intent)
+        }
+
+        btnProfile.setOnClickListener {
+            var intent: Intent = Intent(this, perfilUsuario::class.java)
+            intent.putExtra("tipoMascota","Interes")
+            startActivity(intent)
+        }
+
+    }
+
+//    private fun createPolylines() {
+//        val polylineOptions = PolylineOptions()
+//            .add(LatLng())
+//    }
+
+
+
+    private fun createMapFragment() {
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    private fun isLocationPermissionGranted() = ContextCompat.checkSelfPermission(
+        this,
+        android.Manifest.permission.ACCESS_FINE_LOCATION
+    ) == PackageManager.PERMISSION_GRANTED
+
+    private fun enableLocation() {
+        if (!::map.isInitialized) return
+        if (isLocationPermissionGranted()) {
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return
+            }
+            map.isMyLocationEnabled = true
+        } else {
+            requestLocationPermission()
+        }
+    }
+
+    private fun requestLocationPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            )
+        ) {
+            Toast.makeText(this, "Ve a ajustes y acepta los permisos", Toast.LENGTH_SHORT).show()
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                REQUEST_CODE_LOCATION
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            REQUEST_CODE_LOCATION -> if (grantResults.isNotEmpty() && grantResults[0]==PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return
+                }
+                map.isMyLocationEnabled = true
+            } else {
+                Toast.makeText(
+                    this,
+                    "Para activar la localización ve a ajustes y acepta los permisos",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else -> {}
+        }
+    }
+
+    override fun onResumeFragments() {
+        super.onResumeFragments()
+        if (!::map.isInitialized) return
+        if(!isLocationPermissionGranted()) {
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return
+            }
+            map.isMyLocationEnabled = false
+            Toast.makeText(this, "Para activar la localización ve a ajustes y acepta los permisos", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onMyLocationButtonClick(): Boolean {
+        Toast.makeText(this, "Botón pulsado", Toast.LENGTH_SHORT).show()
+        return false
+    }
+
+    override fun onMyLocationClick(p0: Location) {
+        Toast.makeText(this, "Estás en ${p0.latitude}, ${p0.longitude}", Toast.LENGTH_SHORT).show()
     }
 }
